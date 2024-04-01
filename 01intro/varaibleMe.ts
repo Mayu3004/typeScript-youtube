6866eb6433057a265f5401b57cecd29a05f146b7
mayur.limbhore-coditas <mayur.limbhore@coditas.com>
2024-04-01 15:20:11 +0530

    Numbers boolean and inference

--- 01intro/varaibleMe.ts
+++ 01intro/varaibleMe.ts
@@ -1,5 +1,15 @@
 let greetings: string = "Hello Mayur";
 
-let num:number = 6;
+let num: number = 6;
+
+//number
+let num1: number = 334455.3;
+
+//better way =>
+let userId = 1234;
+//userId = "mary"
+
+//boolean
+let isLoggedIn: boolean = false;
 console.log(greetings);
 export {}
