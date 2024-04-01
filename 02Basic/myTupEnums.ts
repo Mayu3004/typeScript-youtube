71a89bc3e50e95d11c6864c7e3254c3bd372bb4c
mayur.limbhore-coditas <mayur.limbhore@coditas.com>
2024-04-01 17:36:57 +0530

    Tuples types

---
+++ 02Basic/myTupEnums.ts
@@ -0,0 +1,19 @@
+// const user: (string | number)[] = [1, "hc"]
+let tUser: [string, number, boolean]
+
+tUser = ["ml", 131, true]
+// real time example
+let rgb: [number, number, number] = [255, 123, 112]
+
+type User = [number, string]
+
+const newUser: User = [112, "Mayur@google.com"]
+
+newUser[1] = "ml.com"
+//newUser.push(true)
+
+
+
+
+
+export {}
