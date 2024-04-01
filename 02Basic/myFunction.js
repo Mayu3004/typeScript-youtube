deb4a34d2ccc8c612539de2be59869247834cd77
mayur.limbhore-coditas <mayur.limbhore@coditas.com>
2024-04-01 16:04:01 +0530

    basic of functions

---
+++ 02Basic/myFunction.js
@@ -0,0 +1,16 @@
+//Not a good way to do this
+// function addTwo(num){
+//     return num + 2
+// }
+// addTwo(5);
+function addTwo(num) {
+    return num + 2;
+}
+addTwo(5);
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+function signUpser(name, email, password) {
+}
+loginUser('h', 'm@m.com');
+signUpser("Mayur", "mayur@12.com", 3);
---
+++ 02Basic/myFunction.ts
@@ -0,0 +1,18 @@
+//Not a good way to do this
+// function addTwo(num){
+//     return num + 2
+// }
+// addTwo(5);
+
+function addTwo(num: number){
+    return num + 2
+}
+addTwo(5);
+let loginUser = (name: string,email: string,isPaid: boolean = false) =>{
+
+}
+function signUpser(name: string,email: string,password: number){
+
+}
+loginUser('h','m@m.com')
+signUpser("Mayur","mayur@12.com",3);
