3ef0a88e582a765d01151ec7da9d162848870e80
mayur.limbhore-coditas <mayur.limbhore@coditas.com>
2024-04-01 17:11:06 +0530

    Arrays

---
+++ 02Basic/myArray.js
@@ -0,0 +1,11 @@
+var superHeros = [];
+// const superPowwer: number[] = [];
+var superPowwer = [];
+superHeros.push('spiderman');
+superPowwer.push(2);
+var allUsers = [];
+allUsers.push({ name: "Mayur", isActive: true });
+var MLModels = [
+    [255, 255, 255],
+    []
+];
---
+++ 02Basic/myArray.ts
@@ -0,0 +1,19 @@
+const superHeros: string[] = [];
+// const superPowwer: number[] = [];
+const superPowwer: Array<number> = [];
+superHeros.push('spiderman');
+superPowwer.push(2);
+
+type User = {
+    name: string
+    isActive: boolean 
+}
+const allUsers: User[] = [];
+
+allUsers.push({name:"Mayur",isActive: true})
+
+const MLModels: number[][] = [
+    [255, 255, 255],
+    []
+]
+
