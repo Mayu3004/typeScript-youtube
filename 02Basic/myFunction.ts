a6a3592e48122b0231db17f380b1fd2867edb7dc
mayur.limbhore-coditas <mayur.limbhore@coditas.com>
2024-04-01 16:17:14 +0530

    better way to write functions

--- 02Basic/myFunction.ts
+++ 02Basic/myFunction.ts
@@ -5,7 +5,8 @@
 // addTwo(5);
 
 function addTwo(num: number){
-    return num + 2
+    // return num + 2
+    return "hello"
 }
 addTwo(5);
 let loginUser = (name: string,email: string,isPaid: boolean = false) =>{
@@ -16,3 +17,30 @@ function signUpser(name: string,email: string,password: number){
 }
 loginUser('h','m@m.com')
 signUpser("Mayur","mayur@12.com",3);
+
+// function getValue(myVal: number): string{
+//     if(myVal>5){
+//         return true;
+//     }
+//     return "200 OK"
+// }
+
+
+const getHello =(s: string):string =>{
+    return ""
+}
+const heros = ['thor','spiderman','ironman'];
+heros.map((hero): string=>{
+    return `hero is ${hero}`;
+})
+
+function consoleError(errmsg: string): void{
+    console.log(errmsg)
+}
+
+function handleError(errmsg: string): never{
+    throw new Error(errmsg)
+}
+
+
+export{}
